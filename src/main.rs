abf29982116c5bec8efd2871ef08525e16256cfb
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-27 17:03:07 +0300

    triagnle
    adding triangle

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod shape;
 
-use crate::shape::{Shape, Rectangle, Circle};
+use crate::shape::{Shape, Rectangle, Circle, Triangle};
 
 fn main() {
     let rectangle = Rectangle::new(5.0, 10.0);
     let circle = Circle::new(7.5);
-    let shapes: Vec<Box<dyn Shape>> = vec![Box::new(rectangle), Box::new(circle)];
+    let triangle = Triangle::new(6.0,5.0);
+    let shapes: Vec<Box<dyn Shape>> = vec![Box::new(rectangle), Box::new(circle),Box::new(triangle)];
 
     for shape in shapes {
-        println!("Area:{:2}", shape.area());
+        println!("Area:{:?}", shape.area());
     }
 }
--- src/shape.rs
+++ src/shape.rs
@@ -37,6 +37,30 @@ impl Shape for Circle {
     }
 }
 
+pub struct Triangle {
+    base: f64,
+    height: f64,
+}
+
+impl Triangle {
+    pub fn new(base: f64, height: f64) -> Triangle {
+        Triangle { base, height }
+    }
+}
+
+impl Shape for Triangle{
+    fn area(&self) -> f64 {
+        0.5*self.base*self.height
+    }
+}
+
+
+
+
+
+
+
+
 
 /*******
 2-Open/Closed Principle:We've designed our app to be open to expansion. We will be able to create new
